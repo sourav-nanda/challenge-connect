fe50f1c0a9bea8fb37291c3e3d60eb7fb89ad062
Sourav Nanda <souravnanda41@gmail.com>
2023-08-01 21:19:33 +0530

    removed templates and static files for api app

--- api/forms/hackathon_form.py
+++
@@ -1,8 +0,0 @@
-# main_app/forms.py
-from django import forms
-from ..models import Hackathon
-
-class HackathonForm(forms.ModelForm):
-    class Meta:
-        model = Hackathon
-        fields = ['title', 'description', 'background_image', 'hackathon_image', 'type_of_submission', 'start_datetime', 'end_datetime', 'reward_prize']
