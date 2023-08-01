d1dcd1c658ab545e6cc6eee88eda28434c40f892
Sourav Nanda <souravnanda41@gmail.com>
2023-08-01 20:16:09 +0530

    added example for creating a hackathon

---
+++ examples/create_hackathon.py
@@ -0,0 +1,32 @@
+import requests
+from pprint import pprint
+from requests.auth import HTTPBasicAuth
+
+endpoint = 'http://localhost:8000/api/hackathons/create/'
+
+auth = HTTPBasicAuth('admin', 'admin')
+
+files = {
+    'background_image': open(r"C:\Users\Sourav\Downloads\challengeConnect_logo.png", 'rb'),
+    'hackathon_image': open(r"C:\Users\Sourav\Downloads\challengeConnect_logo.png", 'rb'),
+}
+
+data = {
+    'title': 'Test Hackathon',
+    'description': 'Testing submission types',
+    'type_of_submission': 'link',  
+    'start_datetime': '2020-08-01T12:00:00Z',
+    'end_datetime': '2020-08-07T12:00:00Z',
+    'reward_prize': '1000 USD',
+}
+
+response = requests.post(endpoint, data=data, files=files,auth=auth)
+
+
+if response.status_code == 201:
+    print('Hackathon created successfully!')
+    pprint(response.json()) 
+else:
+    print('Failed to create hackathon.')
+    print(response.status_code)  
+    pprint(response.text)  
